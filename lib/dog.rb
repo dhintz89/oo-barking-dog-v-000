bcbd866e6d4e0147f4e6f02dea516267ab0b90c4
Dan Hintz <dhintz@live.com>
2018-08-16 01:13:39 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
 # Your code goes here!
 class Dog
 
-  def name=(name)
-    @name = name
+  def name=(new_name)
+    @name = new_name
   end
   
   def name
-    @name = name
+    @name
   end
 
   
